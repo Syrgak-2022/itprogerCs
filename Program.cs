3bfeb13f0391950fa44ebd301dc23042777941d1
Сыргак Иманкулов <syrgakimankulov@gmail.com>
2022-08-14 21:37:07 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine(Math.Ceiling(4.56));
+// Console.WriteLine(Math.Floor(4.4));
+// Console.WriteLine(Math.Round(3.49));
+
+// Прога, в котором вводишь 2 числа, а прога определяет максимальное введенное число
+// int user_input1;
+// int user_input2;
+// user_input1 = Convert.ToInt32(Console.ReadLine());
+// user_input2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Math.Max(user_input1, user_input2));
+
+
+// Возведение в степень
+// Console.WriteLine(Math.Pow(5, 3));
+
